35e61d147758546c20b38b1c57a65fff365e5c41
Ericsson <shevru02@mail.ru>
2021-12-04 23:05:20 +1000

    nav drawer

--- app/src/main/java/com/example/testactivityandroid_9/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/testactivityandroid_9/ui/home/HomeFragment.java
@@ -1,24 +1,28 @@
 package com.example.testactivityandroid_9.ui.home;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.testactivityandroid_9.R;
 import com.example.testactivityandroid_9.databinding.FragmentHomeBinding;
+import com.example.testactivityandroid_9.ui.restaurants.RestaurantsActivity;
 
 public class HomeFragment extends Fragment {
-
     private HomeViewModel homeViewModel;
     private FragmentHomeBinding binding;
+    ImageButton podkrepizza_imagebutton;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -28,13 +32,24 @@ public class HomeFragment extends Fragment {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textHome;
+
+/*        podkrepizza_imagebutton = (ImageButton) getActivity().findViewById(R.id.podkrepizza_imagebutton);
+        podkrepizza_imagebutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity().getApplicationContext(), RestaurantsActivity.class);
+                startActivity(intent);
+            }
+        });*/
+
+
+/*        final TextView textView = binding.textHome;
         homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
             }
-        });
+        });*/
         return root;
     }
 
