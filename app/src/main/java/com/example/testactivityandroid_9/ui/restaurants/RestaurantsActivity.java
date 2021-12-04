c347a1a98d229b44c5fba32b200f274dc328b415
Ericsson <shevru02@mail.ru>
2021-12-04 19:17:29 +1000

    /

---
+++ app/src/main/java/com/example/testactivityandroid_9/ui/restaurants/RestaurantsActivity.java
@@ -0,0 +1,17 @@
+package com.example.testactivityandroid_9.ui.restaurants;
+
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.testactivityandroid_9.R;
+
+public class RestaurantsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurants);
+    }
+}
