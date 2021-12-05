adbf5a95b79a198a24e2f4e5aeeef97afa17576b
Ericsson <shevru02@mail.ru>
2021-12-05 16:30:45 +1000

    add signup activity

---
+++ app/src/main/java/com/example/testactivityandroid_9/ui/login/signup/Login_SignupActivity.java
@@ -0,0 +1,15 @@
+package com.example.testactivityandroid_9.ui.login.signup;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.testactivityandroid_9.R;
+
+public class Login_SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_signup);
+    }
+}
