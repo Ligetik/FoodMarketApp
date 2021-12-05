f8e837be900dd871f7ed1b3d6703199a38c5480e
Ericsson <shevru02@mail.ru>
2021-12-05 15:18:30 +1000

    rename activity custom to callback

--- app/src/main/java/com/example/testactivityandroid_9/ui/callback/CallBackActivity.java
+++ app/src/main/java/com/example/testactivityandroid_9/ui/callback/CallBackActivity.java
@@ -1,4 +1,4 @@
-package com.example.testactivityandroid_9.ui.custom;
+package com.example.testactivityandroid_9.ui.callback;
 
 import android.os.Bundle;
 import android.view.View;
@@ -7,13 +7,13 @@ import android.widget.ImageButton;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.testactivityandroid_9.R;
 
-public class CustomActivity extends AppCompatActivity {
+public class CallBackActivity extends AppCompatActivity {
     ImageButton imageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_custom);
+        setContentView(R.layout.activity_callback);
 
         clickBtnBack();
     }
